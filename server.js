a1e98ab345331871aba5eab62eda0e01375ea496
dshaw1242 <david.shaw1242@gmail.com>
2021-06-06 12:36:55 -0600

    started handlebars template

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening '));
