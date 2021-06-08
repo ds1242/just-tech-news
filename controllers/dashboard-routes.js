63a9886b2a9f19b473b6c9af73b377440782e082
dshaw1242 <david.shaw1242@gmail.com>
2021-06-08 17:20:46 -0600

    added ability to delete and edit posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,6 +40,49 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          ],
+          include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        })
+        .then(dbPostData => {
+            if(!dbPostData){
+                res.status(404).json({message: 'Unable to find post'});
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            })
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Something went wrong trying to find the post'});
+        })
+})
 
 
 module.exports = router;
